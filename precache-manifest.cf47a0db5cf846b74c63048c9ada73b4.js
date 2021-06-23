1e06badc013ed6e5d406593affc9f892a4af72be
ekkaratp <ekkaratp@users.noreply.github.com>
2021-06-23 08:04:36 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.cf47a0db5cf846b74c63048c9ada73b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8c6d971d0f612a155ec60482477395",
+    "url": "https://ekkaratp.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3c4c3074cc174ab49c87",
+    "url": "https://ekkaratp.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "53bdf93dd8f609fa70c3",
+    "url": "https://ekkaratp.github.io/task-management-frontend/static/js/2.eeae3612.chunk.js"
+  },
+  {
+    "revision": "3c4c3074cc174ab49c87",
+    "url": "https://ekkaratp.github.io/task-management-frontend/static/js/main.43246af7.chunk.js"
+  },
+  {
+    "revision": "5fb7165bb0005a21c649",
+    "url": "https://ekkaratp.github.io/task-management-frontend/static/js/runtime~main.ee2c035b.js"
+  }
+]);
